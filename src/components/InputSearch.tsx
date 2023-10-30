035af03cf0c326816b7f7983d532baf05964e015
Max Gill <mgill@cantaloupe.com>
2023-10-30 03:14:35 +0200

    Bug fix disabled searchText

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -3,11 +3,12 @@ import { Flex, Input } from 'antd';
 import { ChangeEvent, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchText } from '../store/taskSlice';
-import { selectTasks } from '../store';
+import { selectSearchText, selectTasks } from '../store';
 
 export const InputSearch = () => {
   const dispatch = useDispatch();
   const listTask = useSelector(selectTasks);
+  const searchText = useSelector(selectSearchText);
 
   const onChangeCaption = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     dispatch(setSearchText(event.target.value));
@@ -17,7 +18,7 @@ export const InputSearch = () => {
       <Flex justify={'flex-end'} align={'center'}  style={{ width: '45%' }}>
           <Input style={{ width: '50%' }}
                  onChange={onChangeCaption}
-                 disabled={!listTask.length}
+                 disabled={!searchText && !listTask.length}
                  size={'large'} allowClear placeholder={'search'}  />
       </Flex>
   );
